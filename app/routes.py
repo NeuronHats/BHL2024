c657231fec57f5f29f2d74cf198ae6aa958b8584
bberni <michalb675@gmail.com>
2024-04-20 11:47:00 +0200

    add connection string

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,7 @@
 from app import app, db
 import sqlalchemy as sa
 from flask import render_template, redirect, url_for, request
+
+@app.route("/")
+def index():
+    return "ok"
--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config: 
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev"
     TEMPLATES_AUTO_RELOAD = True
-    SQLALCHEMY_DATABASE_URI = ""
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:tvKklZfjAwImjUarIHxALIpLIbkvseVO@postgres.railway.internal:5432/railwaytvKklZfjAwImjUarIHxALIpLIbkvseVO"
