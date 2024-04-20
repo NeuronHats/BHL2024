7c122dbb3ee56f1d5e613657e347f18938d14a37
bberni <michalb675@gmail.com>
2024-04-20 14:43:02 +0200

    add migrations

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev"
     TEMPLATES_AUTO_RELOAD = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:DQLeFucvtfWHrsEBwSdYMgptjKJrPapf@roundhouse.proxy.rlwy.net:48825/railway"
+    SQLALCHEMY_DATABASE_URI = "sqlite+libsql://bhl2024-bberni.turso.io/?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTM2MTY3OTQsImlkIjoiYjllZTU2YTgtYzkzMi00NTg1LWFmMzEtMjZlMTk2ZTBkZjI3In0.fBF-4dtac4bZv2SU_urIvE12Jx22uju9Jd3-fKSIuD8JjLAdAWeMxYWgZT6UJirfBtqp5STbyPcQFMeWYNOiDw&secure=true"
