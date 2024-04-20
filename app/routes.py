a3627588471c8aa691f439ccb8d1ffe8cc5ea8ff
Ernest Gupik <ernestgupik@wp.pl>
2024-04-20 13:48:31 +0200

    User login route

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,34 @@
 from app import app, db
+from app.models import User
+from app.forms import LoginForm
 import sqlalchemy as sa
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash
+from flask_login import current_user, logout_user, login_user
 
 
 @app.route("/")
 def index():
     return render_template("base.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
+        )
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
