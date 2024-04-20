4c9e333fd25e83d0abed27029b1cba71a6a29d08
bberni <michalb675@gmail.com>
2024-04-20 14:01:38 +0200

    fix form

--- app/forms.py
+++ app/forms.py
@@ -6,11 +6,5 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
     password_hash = PasswordField("Password", validators=[DataRequired()])
-    education_text = StringField("Education text", validators=[DataRequired()])
-    education_level = StringField("Education level", validators=[DataRequired()])
-    experience_text = StringField("Experience text", validators=[DataRequired()])
-    experience_years = StringField("Experience years", validators=[DataRequired()])
-    technologies_text = StringField("Technologies text", validators=[DataRequired()])
-    soft_skills_text = StringField("Soft skills text", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
