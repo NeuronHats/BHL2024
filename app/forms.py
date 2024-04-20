97fea537cd6ad3e7f70ec461fcc02d30fe11e2aa
bberni <michalb675@gmail.com>
2024-04-20 14:04:48 +0200

    add registration form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
+from app import db
+from app.models import User
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+import sqlalchemy as sa
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +11,17 @@ class LoginForm(FlaskForm):
     password_hash = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField("Repeat Password", validators=[
+                              DataRequired(), EqualTo("password")])
+
+    def validate_email(self, email):
+        email = db.session.scalar(sa.select(User).where(
+            User.email == email.data
+        ))
+        if email is not None:
+            raise ValidationError("Please use a different email address.")
